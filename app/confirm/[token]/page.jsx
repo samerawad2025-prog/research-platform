9a5d695ba9be30aeb54648c55f50c3d70b838581
samerawad2025-prog <samerawad2025@gmail.com>
2026-09-12 00:09:53 +0200

    Delete app directory

--- app/confirm/[token]/page.jsx
+++
@@ -1,8 +0,0 @@
-import ConfirmationScreen from '../../../components/ConfirmationScreen'
-
-export const dynamic = 'force-dynamic'
-
-export default async function ConfirmPage({ params }) {
-  const { token } = await params
-  return <ConfirmationScreen token={token} />
-}
